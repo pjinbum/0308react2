95d01e4b4f5ebfaa78bbb1ea46de394309cad285
Insight <119594529+pjinbum@users.noreply.github.com>
2023-03-08 17:11:43 +0900

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,17 @@
+/* eslint-disable */
+import logo from './logo.svg';
+import './App.css';
+import Comment from './component/Comment';
+import CommentList from './component/CommentList'
+import AddPractice from './component/AddPractice';
+
+function App() {
+  return (
+    <div className='App'>
+      <CommentList/>
+     
+    </div>
+  );
+}
+
+export default App;
